414ac43ed9dc90f63fa3308ad8f40b4049bdc2a8
Krosiber <halileymensahin456@gmail.com>
2024-07-25 15:26:08 +0300

    update footer
    update footer

--- src/app/components/Layout/Footer/index.tsx
+++ src/app/components/Layout/Footer/index.tsx
@@ -15,31 +15,36 @@ interface FooterData {
 const Footer: React.FC<FooterData> = ({ title, WebDeveloper,SosyalMedya, Email, gmail }) => {
   return (
     <footer>
-      <div className='border-t border-[#383838] mb-36 my-36 py-24 text-gray-200 flex flex-col md:flex-row justify-center items-center'>
-        <div className='flex flex-col md:flex-row space-y-10 md:space-y-0 md:space-x-40'>
-          <div className='text-center md:text-left'>
-            <h1 className='text-3xl'>{title}</h1>
-            <br />
-            <span className='text-xl flex'> <FaDev className='mt-1.5 mr-2' />{WebDeveloper}</span>
-          </div>
-          <div className='text-center md:text-left'>
-            <h1 className='text-3xl mb-4'>{SosyalMedya}</h1>
-            <span className='flex justify-center gap-2'>
-              <Link href="https://www.instagram.com/krosiber/"><FaInstagram className='w-5 h-5 hover:text-gray-400' /></Link>
-              <Link href="https://github.com/Krosiber"><FaGithub className='w-5 h-5 hover:text-gray-400' /></Link>
-            </span>
-
-          </div>
-
-          <div className='text-2xl md:justify-start mt-3'>
-            <h1 className='flex items-center'>
-              <CgMail className='w-5 h-5 mr-2' />{Email}
-            </h1>
-            <span className='text-sm'>{gmail}</span>
-          </div>
-        </div>
+  <div className='border-t border-[#383838] my-12 py-8 text-gray-200'>
+    <div className='flex flex-col items-center space-y-8'>
+      <div className='text-center'>
+        <h1 className='text-2xl mb-2'>{title}</h1>
+        <span className='text-lg flex items-center justify-center'>
+          <FaDev className='mr-2' />{WebDeveloper}
+        </span>
       </div>
-    </footer>
+      
+      <div className='text-center'>
+        <h1 className='text-2xl mb-4'>{SosyalMedya}</h1>
+        <span className='flex justify-center gap-4'>
+          <Link href="https://www.instagram.com/krosiber/">
+            <FaInstagram className='w-6 h-6 hover:text-gray-400' />
+          </Link>
+          <Link href="https://github.com/Krosiber">
+            <FaGithub className='w-6 h-6 hover:text-gray-400' />
+          </Link>
+        </span>
+      </div>
+      
+      <div className='text-center'>
+        <h1 className='text-xl flex items-center justify-center mb-2'>
+          <CgMail className='w-5 h-5 mr-2' />{Email}
+        </h1>
+        <span className='text-sm'>{gmail}</span>
+      </div>
+    </div>
+  </div>
+</footer>
   )
 }
 
